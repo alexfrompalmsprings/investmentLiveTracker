c24252020084ebc336fefa984cb39a51e432a266
AlexAvila <alexavila.oc@gmail.com>
2020-03-01 20:12:13 -0800

    app.jsx !

---
+++ client/src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+  }
+
+
+
+  render() {
+    return (
+
+      <div>
+        <div>
+        Hello!!
+        </div>
+      </div>
+
+      );
+  }
+}
+
+export default App;
