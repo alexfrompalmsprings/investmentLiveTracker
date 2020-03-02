deb979c098d9c689939efaf6e002c52293e5431d
AlexAvila <alexavila.oc@gmail.com>
2020-03-01 20:13:32 -0800

    hardcoded api links to the live prices

---
+++ db/apilinks.js
@@ -0,0 +1,24 @@
+var apiLinks = {
+  BITCOIN:'https://api.cryptonator.com/api/ticker/btc-usd',
+
+  RIPPLE :'https://api.cryptonator.com/api/ticker/xrp-usd',
+
+  BITCOIN_CASH: 'https://api.cryptonator.com/api/ticker/bch-usd',
+
+  DASH: 'https://api.cryptonator.com/api/ticker/dash-usd',
+
+  DOGE: 'https://api.cryptonator.com/api/ticker/doge-usd',
+
+  ETHEREUM:'https://api.cryptonator.com/api/ticker/eth-usd',
+
+  ETHEREUM_CLASSIC: 'https://api.cryptonator.com/api/ticker/etc-usd',
+
+  LITECOIN:'https://api.cryptonator.com/api/ticker/ltc-usd',
+
+  MONERO:'https://api.cryptonator.com/api/ticker/xmr-usd',
+
+  Z_CASH:'https://api.cryptonator.com/api/ticker/zec-us'
+
+}
+
+console.log(apiLinks.BITCOIN)
