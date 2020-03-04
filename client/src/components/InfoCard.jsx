93c5b2db28a3ea660ff7a7af38cf938d221b22ed
AlexAvila <alexavila.oc@gmail.com>
2020-03-03 18:20:25 -0800

    last minute changes

--- client/src/components/InfoCard.jsx
+++ client/src/components/InfoCard.jsx
@@ -1,4 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ClassAddCart = styled.div`
+  display:block;
+  justify-content: space-between;
+  width:700px;
+`
+
+const Pic_body = styled.div`
+  width: 16px;
+  opacity :0.2;
+`
+const Priced_usd = styled.span`
+  display:block;
+  color:blue;
+  font-size:.6em;
+`
+
 
 const InfoCard = (props) => {
   // console.log('props getting passed', props)
@@ -8,18 +26,25 @@ const InfoCard = (props) => {
   // console.log('should have only numbers here', value)
 
   return (
-    <div className="custom-card">
-        <div className="card-body">
-        <img src={props.src} alt={props.src} className = "img-responsive float-right"  />
+    // <ClassAddCart>
 
-            <h6 className="card-title"> {props.header} </h6>
 
-            <h2 className="text-primary">${value}</h2>
-            <p className="card-text"><small>{props.label}</small></p>
-            {/* <h6 className="time"> {timeStamp.time} </h6> */}
+      <div className="custom-card">
+          <div className="card-body">
+            <img src={props.src} alt={props.src} className = "img-responsive float-right"  />
+            <h5 className="card-title"> {props.header} </h5>
+          </div>
+          <div>
+              <h2 className="text-primary">${value}</h2>
 
-        </div>
-    </div>
+              <Priced_usd>
+              {props.label}
+              </Priced_usd>
+              {/* <h6 className="time"> {timeStamp.time} </h6> */}
+
+          </div>
+      </div>
+    // </ClassAddCart>
   )
 };
 
